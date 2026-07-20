01380ab738c1c76cc89c3ce3404fba53b20f2085
gawainxu <xgawain@gmail.com>
2026-07-20 11:32:32 +0200

    cl experiments

--- run2.sh
+++ run2.sh
@@ -13,9 +13,16 @@
 #python3 Toy_train.py --experiment_name "E2" --dataset "toy" --classes_idx 1 --model_name "cnn" --losses_path "cnn_toy_E2"
 
 
-python3 main_ce.py --batch_size 64 --epochs 300 --learning_rate 0.1 --cosine True --lr_decay_rate 0.8 --model "resnet18" --datasets "cifar100_marco" --trail 0
-python3 main_ce.py --batch_size 64 --epochs 300 --learning_rate 0.1 --cosine True --lr_decay_rate 0.8 --model "resnet18" --datasets "cifar100_marco" --trail 1
-python3 main_ce.py --batch_size 64 --epochs 300 --learning_rate 0.1 --cosine True --lr_decay_rate 0.8 --model "resnet18" --datasets "cifar100_marco" --trail 2
-python3 main_ce.py --batch_size 64 --epochs 300 --learning_rate 0.1 --cosine True --lr_decay_rate 0.8 --model "resnet18" --datasets "cifar100_marco" --trail 3
+#python3 main_ce.py --batch_size 64 --epochs 300 --learning_rate 0.1 --cosine True --lr_decay_rate 0.8 --model "resnet18" --datasets "cifar100_marco" --trail 0
+#python3 main_ce.py --batch_size 64 --epochs 300 --learning_rate 0.1 --cosine True --lr_decay_rate 0.8 --model "resnet18" --datasets "cifar100_marco" --trail 1
+#python3 main_ce.py --batch_size 64 --epochs 300 --learning_rate 0.1 --cosine True --lr_decay_rate 0.8 --model "resnet18" --datasets "cifar100_marco" --trail 2
+#python3 main_ce.py --batch_size 64 --epochs 300 --learning_rate 0.1 --cosine True --lr_decay_rate 0.8 --model "resnet18" --datasets "cifar100_marco" --trail 3
 
 #python3 main_linear.py --model "resnet18" --datasets "cifar100_marco" --trail 7 --backbone_model_direct "/save/CE/cifar100_marco_models/cifar100_marco_resnet18_1trail_0_128_128" --backbone_model_name "last.pth"
+
+python3 Toy_metrics.py --feature_path1 "./features/toy_toy_E1_task_0_data_0_train" --feature_path2 "./features/toy_toy_E3_task_2_task_2_data_0_train" --num_classes 2 --feature_name "linear1"
+python3 Toy_metrics.py --feature_path1 "./features/toy_toy_E1_task_0_data_0_train" --feature_path2 "./features/toy_toy_E4_task_2_task_2_data_0_train" --num_classes 2 --feature_name "linear1"
+python3 Toy_metrics.py --feature_path1 "./features/toy_toy_E1_task_0_data_0_train" --feature_path2 "./features/toy_toy_E5_task_2_task_2_data_0_train" --num_classes 2 --feature_name "linear1"
+python3 Toy_metrics.py --feature_path1 "./features/toy_toy_E2_task_0_data_0_train" --feature_path2 "./features/toy_toy_E6_task_2_task_2_data_0_train" --num_classes 2 --feature_name "linear1"
+python3 Toy_metrics.py --feature_path1 "./features/toy_toy_E2_task_0_data_0_train" --feature_path2 "./features/toy_toy_E7_task_2_task_2_data_0_train" --num_classes 2 --feature_name "linear1"
+python3 Toy_metrics.py --feature_path1 "./features/toy_toy_E2_task_0_data_0_train" --feature_path2 "./features/toy_toy_E8_task_2_task_2_data_0_train" --num_classes 2 --feature_name "linear1"
