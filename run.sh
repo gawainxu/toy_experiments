db928c650af18f7035d27972fe34531c070467fc
gawainxu <xgawain@gmail.com>
2026-07-21 21:25:48 +0200

    cl experiments

--- run.sh
+++ run.sh
@@ -60,12 +60,12 @@
 #python3 Toy_metrics.py --feature_path1 "./features/toy_toy_E2_train" --feature_path2 "./features/toy_toy_E8_train" --num_classes 2 --feature_name "conv1"
 
 # continual training for task 2
-python3 Toy_train.py --experiment_name "E3" --dataset "toy" --classes_idx 2 --task_idx 2 --model_name "toy" --last_model_path "./models/toy_toy_E3.pth" --losses_path "toy_toy_E3_task_2"
-python3 Toy_train.py --experiment_name "E4" --dataset "toy" --classes_idx 3 --task_idx 2 --model_name "toy" --last_model_path "./models/toy_toy_E4.pth" --losses_path "toy_toy_E4_task_2"
-python3 Toy_train.py --experiment_name "E5" --dataset "toy" --classes_idx 4 --task_idx 2 --model_name "toy" --last_model_path "./models/toy_toy_E5.pth" --losses_path "toy_toy_E5_task_2"
-#python3 Toy_train.py --experiment_name "E6" --dataset "toy" --classes_idx 5 --task_idx 2 --model_name "toy" --last_model_path "./models/toy_toy_E6.pth" --losses_path "toy_toy_E6_task_2"
-#python3 Toy_train.py --experiment_name "E7" --dataset "toy" --classes_idx 6 --task_idx 2 --model_name "toy" --last_model_path "./models/toy_toy_E7.pth" --losses_path "toy_toy_E7_task_2"
-#python3 Toy_train.py --experiment_name "E8" --dataset "toy" --classes_idx 7 --task_idx 2 --model_name "toy" --last_model_path "./models/toy_toy_E8.pth" --losses_path "toy_toy_E8_task_2"
+python3 Toy_train.py --experiment_name "E3" --dataset "toy" --experiment_idx 2 --task_idx 2 --model_name "toy" --last_model_path "./models/toy_toy_E3.pth" --losses_path "toy_toy_E3_task_2"
+python3 Toy_train.py --experiment_name "E4" --dataset "toy" --experiment_idx 3 --task_idx 2 --model_name "toy" --last_model_path "./models/toy_toy_E4.pth" --losses_path "toy_toy_E4_task_2"
+python3 Toy_train.py --experiment_name "E5" --dataset "toy" --experiment_idx 4 --task_idx 2 --model_name "toy" --last_model_path "./models/toy_toy_E5.pth" --losses_path "toy_toy_E5_task_2"
+#python3 Toy_train.py --experiment_name "E6" --dataset "toy" --experiment_idx 5 --task_idx 2 --model_name "toy" --last_model_path "./models/toy_toy_E6.pth" --losses_path "toy_toy_E6_task_2"
+#python3 Toy_train.py --experiment_name "E7" --dataset "toy" --experiment_idx 6 --task_idx 2 --model_name "toy" --last_model_path "./models/toy_toy_E7.pth" --losses_path "toy_toy_E7_task_2"
+#python3 Toy_train.py --experiment_name "E8" --dataset "toy" --experiment_idx 7 --task_idx 2 --model_name "toy" --last_model_path "./models/toy_toy_E8.pth" --losses_path "toy_toy_E8_task_2"
 
 # finetune to evalute the models after task2
 #python3 Toy_finetune.py --experiment_name "E3" --dataset "toy" --experiment_idx 2 --task_idx_model 2 --task_idx_data 1 --model_name "toy" --last_model_path "./models/toy_toy_E3_task_2.pth"
