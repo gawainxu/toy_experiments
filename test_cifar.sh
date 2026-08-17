bc1549193e37650a82fb0c94531640ebd1209665
gawainxu <xgawain@gmail.com>
2026-08-17 11:52:16 +0200

    train cifar

--- test_cifar.sh
+++ test_cifar.sh
@@ -1,1 +1,9 @@
 python3 main_testing.py --model_path "./save/CE/cifar100_marco_models/cifar100_marco_resnet18_1trail_0_128_128/last.pth" --trail 0
+python3 main_testing.py --model_path "./save/CE/cifar100_marco_models/cifar100_marco_resnet18_1trail_1_128_128/last.pth" --trail 1
+python3 main_testing.py --model_path "./save/CE/cifar100_marco_models/cifar100_marco_resnet18_1trail_2_128_128/last.pth" --trail 2
+
+python3 main_testing.py --model_path "./save/CE/cifar100_marco_models/cifar100_marco_resnet18_1trail_5_128_128/last.pth" --trail 5
+python3 main_testing.py --model_path "./save/CE/cifar100_marco_models/cifar100_marco_resnet18_1trail_6_128_128/last.pth" --trail 6
+python3 main_testing.py --model_path "./save/CE/cifar100_marco_models/cifar100_marco_resnet18_1trail_7_128_128/last.pth" --trail 7
+
+python3 main_testing.py --model_path "./save/CE/cifar100_marco_models/cifar100_marco_resnet18_1trail_8_128_128/last.pth" --trail 8
