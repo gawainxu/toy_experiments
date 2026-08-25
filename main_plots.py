94ee676e084a6776fd0af354fb87a08b9fdefa51
gawainxu <xgawain@gmail.com>
2026-08-25 22:06:40 +0200

    train imagenet

--- main_plots.py
+++ main_plots.py
@@ -38,8 +38,8 @@ if __name__ == '__main__':
         plt.plot(accs, label=name)
 
     plt.legend()
-    plt.xlabel("Epochs", fontsize=16)
-    plt.ylabel("Accuracy (%)", fontsize=16)
+    plt.xlabel("Epochs", fontsize=20)
+    plt.ylabel("Accuracy (%)", fontsize=20)
     plt.savefig(opt.plot_save_path)
 
 
