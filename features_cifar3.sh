a4b0f7bfd66ae9f1110838ef7356bed6679ab1de
gawainxu <xgawain@gmail.com>
2026-09-02 13:11:48 +0200

    train imagenet

--- features_cifar3.sh
+++ features_cifar3.sh
@@ -146,7 +146,7 @@ python3 feature_reading_old.py --datasets 'cifar100_marco' --model "resnet18" --
 python3 feature_reading_old.py --datasets 'cifar100_marco' --model "resnet18" --trail 1 --model_trail 4 --model_path "/save/CE/cifar100_marco_models/cifar100_marco_resnet18_1trail_4_128_128_last_114/last.pth" --if_train 'test_known'
 
 python3 feature_reading_old.py --datasets 'cifar100_marco' --model "resnet18" --trail 2 --model_trail 4 --model_path "/save/CE/cifar100_marco_models/cifar100_marco_resnet18_1trail_4_128_128_last_115/last.pth" --if_train 'train'
-python3 feature_reading_old.py --datasets 'cifar100_marco' --model "resnet18" --trail 3 --model_trail 4 --model_path "/save/CE/cifar100_marco_models/cifar100_marco_resnet18_1trail_4_128_128_last_115/last.pth" --if_train 'test_known'
+python3 feature_reading_old.py --datasets 'cifar100_marco' --model "resnet18" --trail 2 --model_trail 4 --model_path "/save/CE/cifar100_marco_models/cifar100_marco_resnet18_1trail_4_128_128_last_115/last.pth" --if_train 'test_known'
 
 
 # session 2 models on session 1 data
